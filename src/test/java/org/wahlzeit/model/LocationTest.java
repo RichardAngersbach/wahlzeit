99219f88cd43f3bd6628f5416642d5ef9d58ee0f
Richy <Richy@192.168.2.103>
2017-11-04 15:51:36 +0100

    Minor fixes to Location class.

--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -7,10 +7,12 @@ import static org.junit.Assert.assertTrue;
 
 public class LocationTest {
 	@Test
-	public void compareCoordinateValues() {
+	public void compareLocations() {
 		Location loc = new Location(new Coordinate(0.0, 0.0, 0.0));
-		Location loc2 = new Location(new Coordinate(0.0, 0.0, 0.0));
+		Location loc2 = new Location(null);
+		Location loc3 = new Location(new Coordinate(1.0, 2.0, 3.0));
 		assertTrue(loc.equals(loc2));
 		assertFalse(loc.equals(null));
+		assertFalse(loc.equals(loc3));
 	}
 }
